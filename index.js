41da99c7c6c7c87165507bc75cfc103c342079c8
Duncan <njorogedun870@gmail.com>
2024-07-19 14:28:51 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+// index.js
+
+// Remove the <main> element with id "main"
+document.querySelector('main#main').remove();
+
+// Create a new <h1> element and assign it to the newHeader variable
+window.newHeader = document.createElement('h1');
+window.newHeader.id = 'victory';
+
+// Set the text content of the <h1> element
+window.newHeader.innerHTML = `${'Duncan'} is the champion`;
+
+// Add the new <h1> element to the DOM
+document.body.appendChild(window.newHeader);
